e3bc4c3a3e91ae75acacdb759a22dabf75bb6264
sgadberry <sgadberry@uaex.edu>
2017-12-18 14:46:48 -0600

    Function created to print contents of table19_1 embedded in the sysdata.rda file for reference to breed.codes and breed adjustments.

---
+++ R/table191print.R
@@ -0,0 +1,10 @@
+#' Print Table 19_1 Table Content
+#'
+#' Provides access to the internal data content of Table 19_1
+#'
+#' @return A data frame with the content of Table 19_1 for breed.code reference.
+#' @example Table19_1.print()
+#' @export
+Table19_1.print <- function(){
+  print(table19_1)
+}
